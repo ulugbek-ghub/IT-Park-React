0db5a26bf0534fe1ee2d4a19b9ec9d06183cca37
ulugbek-ghub <ulugbek.github@gmail.com>
2026-02-08 23:39:43 +0500

    3-dars UV

--- 3-dars/src/App.jsx
+++ 3-dars/src/App.jsx
@@ -1,24 +1,30 @@
-import Hellochild from './Components/Hello/hellochild'
-import './App.css'
+import "./App.css";
+import Hellochild from './Components/Hello/hellochild';
 
 function App() {
-  const data = {
-    name: "Jorj Washington",
-    profession: "Prezident",
-    age: 67
-  }
+  const users = [
+    { name: "Jorj Washington", profession: "Prezident", age: 67, img: "Image-1.jpg" },
+    { name: "Elon Musk", profession: "Biznesmen", age: 47, img: "Image-2.jpg" },
+    { name: "Stiv Jobs", profession: "Apple ceo", age: 56, img: "Image-3.jpg" },
+    { name: "Bill Geyts", profession: "MICROsoft Ceo", age: 68, img: "Image-4.jpg" },
+    { name: "Donld Tramp", profession: "Prezident", age: 69, img: "Image-5.jpg" },
+    { name: "Gofur Ruziev", profession: "Dasturchi", age: 33, img: "Image-6.png" },
+    { name: "Shavkat MIrziyoyev", profession: "Prezident", age: 59, img: "Image-7.jpg" }
+  ];
 
   return (
-    <div className="container">
-      <Hellochild name={data.name} profession={data.profession} age={data.age} />
-      <Hellochild name={data.name} profession={data.profession} age={data.age} />
-      <Hellochild name={data.name} profession={data.profession} age={data.age} />
-      <Hellochild name={data.name} profession={data.profession} age={data.age} />
-      <Hellochild name={data.name} profession={data.profession} age={data.age} />
-      <Hellochild name={data.name} profession={data.profession} age={data.age} />
-      <Hellochild name={data.name} profession={data.profession} age={data.age} />
+    <div>
+      {users.map((user, index) => (
+        <Hellochild 
+          key={index}
+          name={user.name} 
+          profession={user.profession} 
+          age={user.age} 
+          userImage={user.img} 
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- 3-dars/src/Components/Hello/hellochild.jsx
+++ 3-dars/src/Components/Hello/hellochild.jsx
@@ -1,15 +1,12 @@
-import viteLogo from '../Hello/vite.svg'
+const Hellochild = ({ name, profession, age, userImage }) => {
+  return (
+    <div className="hello-child">
+      <img src={`/${userImage}`} alt={name} />
+      <h2>Salom, {name}</h2>
+      <p>Professionali: {profession}</p>
+      <p>Yosh: {age}</p>
+    </div>
+  );
+};
 
-const Hellochild = ({name, profession, age}) => {
-    return (
-        <div className="hello-child">
-            <img src={viteLogo} alt="Vite logo" />
-            
-            <h2>Salom, {name}!</h2>
-            <p>Kasbi: {profession}</p>
-            <p>Yoshi: {age}</p>
-        </div>
-    )
-}
-
-export default Hellochild
+export default Hellochild;
