df6eab56c2c553bf9ec2d05ffd4c1e0881cca425
ulugbek-ghub <ulugbek.github@gmail.com>
2026-02-09 16:31:19 +0500

    4-dars React

--- 4-Dars/src/App.jsx
+++ 4-Dars/src/App.jsx
@@ -3,14 +3,48 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const [company, setCompany] = useState("Al-Xorazmiy Vorislari")
 
-  return (
-    <>
-      
-    </>
-  )
-}
+//   function changeCompany() {
+//     console.log("changeCompany ishladi");
+//     setCompany("Yandex Taxi")
+//   }
+
+//   return (
+//     <>
+//       <h1>{company}</h1>
+//       <button onClick={changeCompany}>Click</button>
+//     </>
+//   )
+// }
+
+// export default App
+
+// const App = () => {
+//   const [count, setCount] = useState(0)
+//   return (
+//     <div>
+//       <h1>{count}</h1>
+//       <button onClick={() => setCount(count +1)}>Increment atish</button>
+//       <br />
+//       <br />
+//       <button onClick={() => setCount(count -1)}>Decrement atish</button>
+//     </div>
+//   )
+// }
+
+
+//Matn bor textini koritish va berkitish tugmalari orqali boshqarish
+// const App = () => {
+//   const [show, setShow] = useState(true)
+//   return (
+//     <div>
+//       {show && <h1>Matn bor</h1>}
+//       <button onClick={() => setShow(true)}>Koritish</button>
+//       <button onClick={() => setShow(false)}>Berkitish</button>
+//     </div>
+//   )
+// }
 
 export default App
