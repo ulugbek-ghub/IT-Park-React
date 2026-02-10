8ada0a155fd1a6bb54fc3b5a38e533cacee988c6
ulugbek-ghub <ulugbek.github@gmail.com>
2026-02-10 22:48:08 +0500

    4-Dars UV

--- 4-Dars/src/App.jsx
+++ 4-Dars/src/App.jsx
@@ -47,4 +47,77 @@ import './App.css'
 //   )
 // }
 
+
+//--------------
+
+// UV 
+
+// 1-mashq
+// const App = () => {
+//   const [text, setText] = useState("")
+//   return (
+//     <div>
+//       <input type="text" onChange={(e) => setText(e.target.value)} />
+//       <h1>{text}</h1>
+//     </div>
+//   )
+// }
+
+//2 -mashq
+const App = () => {
+  const [user, setUser] = useState({ name: "Asad", age: "15" })
+
+  return (
+    <div>
+      <h1>Ismi: {user.name}</h1>
+      <h1>Yoshi: {user.age}</h1>
+
+      <input 
+        type="text" 
+        placeholder="Ism yozing" 
+        onChange={(e) => setUser({ ...user, name: e.target.value })} 
+      />
+      <br />
+      <input 
+        type="number" 
+        placeholder="Yosh yozing" 
+        onChange={(e) => setUser({ ...user, age: e.target.value })} 
+      />
+    </div>
+  )
+}
+
+//3 mashq
+// function App() {
+//   const [company, setCompany] = useState("Al-Xorazmiy Vorislari")
+//   function changeCompany() {
+//     setCompany(company === "Al-Xorazmiy Vorislari" ? "Yandex Taxi" : "Al-Xorazmiy Vorislari")
+//   }
+
+//   return (
+//     <>
+//       <h1>{company}</h1>
+//       <button onClick={changeCompany}>Click</button>
+//     </>
+//   )
+// }
+
+//4 mshq
+// 4. Inputga yoz → tugma bos → ekranga chiqsin
+// const App = () => {
+//   const [matn, setmatn] = useState("")
+//   function changeMatn() {
+//     setmatn(document.getElementById("input").value)
+//   }
+//   return (
+//     <div>
+//       <h1>{matn}</h1>
+//       <input type="text" id='input' />
+//       <br />
+//       <button onClick={changeMatn}>Matn chiqarsh</button>
+//     </div>
+//   )
+// }
+
+
 export default App
