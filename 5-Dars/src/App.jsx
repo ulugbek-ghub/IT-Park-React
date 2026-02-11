1b401bfe649c6c72dbe9c9be810fd5dc29371efa
ulugbek-ghub <ulugbek.github@gmail.com>
2026-02-11 16:28:21 +0500

    5-Dars

---
+++ 5-Dars/src/App.jsx
@@ -0,0 +1,58 @@
+import { use, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// function App() {
+//   const [user, setUser] = useState(null)
+//   function logIn() {
+//     setUser({name:"Jorj",role:"admin"})
+//   }
+//   if(!user) {
+//     return (
+//       <div>
+//         <h1>User royhatdan otgan</h1>
+//         <button onClick={logIn}>Lgin</button>
+//       </div>
+//     )
+//   }
+//   return <h1>User bor,User name {user.name}</h1>
+// }
+
+
+// function App() {
+//   const [user, setUser] = useState(null)
+//   function logIn() {
+//     setUser({name:"Trump",role:"admin"})
+//   }
+//   if(!user) {
+//     return (
+//       <div>
+//         <h1>Siz Prezidentmisiz</h1>
+//         <button onClick={logIn}>Albatta, hov Amerikani prezidentiman</button>
+//       </div>aaa
+//   }
+//   return <h1>Xush kelibsiz, oga{user.name}</h1>
+// }
+
+function App() {
+  const [age, setAge] = useState(16)
+  function yosh() {
+    setAge(18)
+  }
+
+  if(age < 18) {
+    return (
+      <div>
+        <h1>Siz 18 yosh emassiz</h1>
+        <button onClick={yosh}>Yoshimni ostir</button>
+      </div>
+    )
+  }
+  return <h1>Siz endi 18 yosh</h1>
+}
+
+
+
+
+export default App
