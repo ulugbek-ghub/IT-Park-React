51fac74686207da2c44f891f5c3160323ef19a0c
ulugbek-ghub <ulugbek.github@gmail.com>
2026-02-12 20:45:10 +0500

    5-Dars

--- 5-Dars/src/App.jsx
+++ 5-Dars/src/App.jsx
@@ -52,7 +52,47 @@ import './App.css'
 //   return <h1>Siz endi 18 yosh</h1>
 // }
 
+//UV
+
+// 1mashq
+// function App() {
+//   const [parol, setParol] = useState("")
+
+//   if(parol === "1234") {
+//     return <div>Togri parol</div>
+//   }
+  
+//   return (
+//     <div>
+//       <input 
+//         type="password" 
+//         value={parol}
+//         onChange={(e) => setParol(e.target.value)}
+//         placeholder="Parol kiriting"
+//       />
+//       <div>Notogri parol</div>
+//     </div>
+//   )
+// }
 
 
+//2 mshq
+
+// function App() {
+//     const [role, setRole] = useState("Guest")
+//     if(role === "Admin") {
+//         return <div>Admin panel</div>
+//     }
+//     if(role === "User") {
+//         return <div>User Panel</div>
+//     }
+//     return (
+//         <div>
+//             <h1>Guest</h1>
+//             <button onClick={() => setRole("Admin")}>Admin</button>
+//             <button onClick={() => setRole("User")}>User</button>
+//         </div>
+//     )
+// }
 
 export default App
