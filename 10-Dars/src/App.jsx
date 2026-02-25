5bf387743817b3a94c2e4737c81cca64857aa14d
ulugbek-ghub <ulugbek.github@gmail.com>
2026-02-25 15:35:22 +0500

    10-Dars

--- 10-Dars/src/App.jsx
+++ 10-Dars/src/App.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+
+// useReducer --- useState ga o'xshash, lekin stateni boshqarish uchun ko'proq mantiqni qo'llash imkonini beradi,
+// 3 ta argument qabul qiladi: reducer function, boshlang'ich state va (ixtiyoriy) init function.
+
+function reducerFn(state, action) {
+  switch (action.type) {
+    case 'INC':
+      return {
+        ...state,
+        count: state.count + 1,
+        even: (state.count + 1) % 2 === 0 && state.count + 1 !== 0
+      }
+    case 'DEC':
+      return {
+        ...state,
+        count: state.count - 1,
+        even: (state.count - 1) % 2 === 0 && state.count - 1 !== 0
+      }
+    default:
+      return state
+  }
+}
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducerFn, { count: 0, even: false })
+
   return (
     <div>
-      
+      <button onClick={() => dispatch({ type: 'DEC' })}>-</button>
+      <span>
+        {state.count} || Juftmi: {state.even ? 'Ha' : 'Yoq'}
+      </span>
+      <button onClick={() => dispatch({ type: 'INC' })}>+</button>
     </div>
   )
 }
+
+
+
 
 export default App
