638e99adb2abb6a1dcdf4d1e2e433f46ca9a9ef1
ulugbek-ghub <ulugbek.github@gmail.com>
2026-03-02 15:35:03 +0500

    12-Dars

--- 12-Dars/src/App.jsx
+++ 12-Dars/src/App.jsx
@@ -2,4 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
 
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
