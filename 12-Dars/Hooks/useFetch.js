c143cab603f9226dfd77229e1c54d4dc482135f2
ulugbek-ghub <ulugbek.github@gmail.com>
2026-03-02 16:30:47 +0500

    12-Dars

--- 12-Dars/Hooks/useFetch.js
+++ 12-Dars/Hooks/useFetch.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import { useState, useEffect } from "react"
 
-const useFetch = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+function useFetch(url) {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    setLoading(true)
+    setError(null)
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Nimadir xato bor")
+        }
+        return response.json()
+      })
+      .then((json) => {
+        setData(json)
+        setLoading(false)
+      })
+      .catch((err) => {
+        setError(err)
+        setLoading(false)
+      })
+  }, [url])
+
+  return [data, loading, error]
 }
 
 export default useFetch
--- 12-Dars/src/App.jsx
+++ 12-Dars/src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import React from 'react'
+import React from "react"
+import useFetch from "../Hooks/useFetch"
 
 const App = () => {
+  const [data, loading, error] = useFetch(
+    "https://jsonplaceholder.typicode.com/todos"
+  )
+
+  if (loading) return <h2>Loading.....</h2>
+  if (error) return <h1>{error.message}</h1>
+
   return (
     <div>
-      
+      <ul>
+        {data?.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </div>
   )
 }
