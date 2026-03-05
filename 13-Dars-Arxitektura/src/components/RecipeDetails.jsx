406113c85ade6d1b831fc81d010ffdadedceb006
ulugbek-ghub <ulugbek.github@gmail.com>
2026-03-05 21:29:51 +0500

    13-Dars UV

--- 13-Dars-Arxitektura/src/components/RecipeDetails.jsx
+++ 13-Dars-Arxitektura/src/components/RecipeDetails.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './RecipeDetails.css'
 
 const RecipeDetails = ({ recipe }) => {
+    const [fullscreen, setFullscreen] = useState(false)
+
     return (
         <div className="recipe-card">
+            {fullscreen && (
+                <div className="fullscreen-overlay" onClick={() => setFullscreen(false)}>
+                    <img src={recipe.image || ""} alt={recipe.name} />
+                </div>
+            )}
             <img src={recipe.image || ""} alt={recipe.name} />
             <div className="recipe-card-body">
                 <div className="recipe-card-title-row">
                     <h2>{recipe.name}</h2>
-                    <button className="btn-save">Save</button>
+                    <button className="btn-save" onClick={() => setFullscreen(true)}>Save</button>
                 </div>
                 <p>{recipe.instructions || recipe.description}</p>
                 <p className="recipe-cook-time">
